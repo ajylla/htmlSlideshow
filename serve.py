23def8a9712cc7ed6e6d93aeba51cbd04e494ed8
ajylla <aleksi.ylilaurila@gmail.com>
2025-05-26 18:29:38 +0300

    Added little explanation for Python http server.

--- serve.py
+++ serve.py
@@ -1,9 +1,14 @@
+"""
+This Python server is required (I think) because CORS policy
+prevents simply fetching files from the file system. Fetching them
+via http requests is a workaround.
+"""
 import http.server
 import socketserver
 
 PORT = 8888
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at port {PORT}")
-    httpd.serve_forever()
+with socketserver.TCPServer(("", PORT), Handler) as server:
+    print(f"Serving at http://localhost:{PORT}")
+    server.serve_forever()
